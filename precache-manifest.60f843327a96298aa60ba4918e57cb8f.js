f883746444c75c47c142d8f3cd1fda13996bd936
Mehul Agrawal <agrawal.mehul92@gmail.com>
2019-05-23 23:48:40 -0700

    Updates

---
+++ precache-manifest.60f843327a96298aa60ba4918e57cb8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e222c847b6e92d69928b8dde1d47fe",
+    "url": "/theNotes/index.html"
+  },
+  {
+    "revision": "ab3f1680d0e960214bb0",
+    "url": "/theNotes/static/css/main.dd80410c.chunk.css"
+  },
+  {
+    "revision": "b00d1f832b38d0584112",
+    "url": "/theNotes/static/js/2.912951aa.chunk.js"
+  },
+  {
+    "revision": "ab3f1680d0e960214bb0",
+    "url": "/theNotes/static/js/main.f34036f3.chunk.js"
+  },
+  {
+    "revision": "f77ed82a732e290157cd",
+    "url": "/theNotes/static/js/runtime~main.05e53e14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/theNotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
